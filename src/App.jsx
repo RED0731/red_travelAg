848bb502fdc9a6621ed3224b3442978cfddf9915
alocparker <alocparker1220@gmail.com>
2023-10-31 08:17:39 +0545

    almost install process has bee completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-      <div>Hello there, This is the main section.</div>
+      <div className="bg-slate-950 w-full min-h-screen text-green-500">Hello there, This is the main section.</div>
     </>
   );
 }
