e5ddb6111bfcc3385db944f2b089dd7343847541
alocparker <alocparker1220@gmail.com>
2023-11-01 13:18:14 +0545

    working on the home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./pages/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <div className="bg-gray-200 w-full min-h-screen overflow-hidden">
           <Header />
+          <Home />
         </div>
       </Router>
     </>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="w-full h-auto">This is Home page section.</div>
+    </>
+  );
+};
+
+export default Home;
