f93c1afafb57a71479a55eeacf9962f940382ebd
alocparker <alocparker1220@gmail.com>
2023-11-01 13:50:11 +0545

    changes the things

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import HeroImg from "../../assets/Hero/hero.png";
 
 const Home = () => {
   return (
     <>
-      <div className="w-full h-auto flex items-center justify-between py-24 px-20 gap-8">
-        <div className="w-1/2 h-auto bg-orange-500 rounded-md p-5">
+      <div className="w-full h-screen flex items-center justify-between py-24 px-20 gap-8">
+        <div className="w-[50%] h-auto bg-orange-500 rounded-md p-5">
           Hello this is content section.
         </div>
-        <div className="w-1/2 h-auto bg-purple-500 rounded-md p-5">
-          Hello this is image section.
+        <div className="w-[35%] h-[65vh] bg-purple-500 rounded-md p-5">
+          <img
+            src={HeroImg}
+            alt="Hero Travel Img"
+            className="w-full h-full object-contain object-center"
+          />
         </div>
       </div>
     </>
