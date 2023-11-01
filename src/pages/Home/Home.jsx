d1dfbaab5e3de91f9e89ae9e368d9b14cc63a07b
alocparker <alocparker1220@gmail.com>
2023-11-01 13:42:04 +0545

    working on the hero content section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="w-full h-auto">This is Home page section.</div>
+      <div className="w-full h-auto flex items-center justify-between py-24 px-20 gap-8">
+        <div className="w-1/2 h-auto bg-orange-500 rounded-md p-5">
+          Hello this is content section.
+        </div>
+        <div className="w-1/2 h-auto bg-purple-500 rounded-md p-5">
+          Hello this is image section.
+        </div>
+      </div>
     </>
   );
 };
