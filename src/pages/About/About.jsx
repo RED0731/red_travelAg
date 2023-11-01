a56fe79bcfbc3ec48eb88b5ced59d53ec5c6d6c3
alocparker <alocparker1220@gmail.com>
2023-11-01 20:58:48 +0545

    changes the things

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="w-full h-auto">This is About us page section.</div>
+      <div className="w-full h-auto py-56">This is About us page section.</div>
     </>
   );
 };
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -18,9 +18,11 @@ const Category = () => {
             Select your Type
           </h1>
         </div>
-        <div className="w-full h-auto flex items-center justify-center">
-          <div className="flex items-center justify-center flex-col text-center bg-white p-5 border border-gray-300 rounded-md shadow-lg">
-            <img src={Beach} alt="" className="w-[70px] h-[70px] object-contain object-center p-2 bg-orange-600 rounded-xl" />
+        <div className="w-full h-auto flex items-center justify-center gap-12">
+          <div className="w-[170px] h-auto flex items-center justify-center flex-col text-center bg-white p-5 border border-gray-300 rounded-lg cursor-pointer shadow-md hover:shadow-lg ease-out duration-500">
+            <img src={Beach} alt="Category Icon" className="w-[70px] h-[70px] object-contain object-center p-1 bg-orange-600/30 rounded-lg mb-2" />
+            <h1 className="text-gray-900 text-[1.2rem] font-bold">Beaches</h1>
+            <p className="text-gray-400 text-base font-normal">10+ Tours</p>
           </div>
         </div>
       </div>
