ea7801896efd53a37e5652bcc01d79d8b005bc0e
alocparker <alocparker1220@gmail.com>
2023-11-03 18:16:29 +0545

    working on the destinations

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -6,10 +6,10 @@ const Destination = () => {
       <div className="w-full h-auto py-16 px-20">
         <div className="text-center mb-10">
           <div className="satisfy-font text-2xl font-medium text-orange-500 text-center">
-            Browse by Category
+            Popular Destinations
           </div>
           <h1 className="text-[2.5rem] text-gray-900 text-center font-black">
-            Select your Type
+            Top Travel Destinations
           </h1>
         </div>
       </div>
