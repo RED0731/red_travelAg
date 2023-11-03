58b19ed75518e33c8961c077c2188eab881b37a8
alocparker <alocparker1220@gmail.com>
2023-11-03 18:45:27 +0545

    working on the destination card

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -12,6 +12,41 @@ const Destination = () => {
             Top Travel Destinations
           </h1>
         </div>
+        <div className="w-full h-auto flex items-center justify-between gap-7">
+          <div className="w-[35%] h-[50vh] rounded-md overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/08/26/17/46/nepal-908837_1280.jpg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div className="w-[20%] h-[50vh] rounded-md overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/08/26/17/46/nepal-908837_1280.jpg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div className="w-[20%] h-[50vh] flex items-center flex-col gap-y-7">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/08/26/17/46/nepal-908837_1280.jpg"
+              alt=""
+              className="w-full h-1/2 object-cover object-center rounded-md"
+            />
+            <img
+              src="https://cdn.pixabay.com/photo/2015/08/26/17/46/nepal-908837_1280.jpg"
+              alt=""
+              className="w-full h-1/2 object-cover object-center rounded-md"
+            />
+          </div>
+          <div className="w-[25%] h-[50vh] rounded-md overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/08/26/17/46/nepal-908837_1280.jpg"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
