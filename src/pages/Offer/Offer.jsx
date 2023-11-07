3c756a8add07babd45b500212385adb5ceafaf3d
alocparker <alocparker1220@gmail.com>
2023-11-07 21:18:10 +0545

    almost completed the offer section

--- src/pages/Offer/Offer.jsx
+++ src/pages/Offer/Offer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import SecondaryButton from "../../components/Button/SecondaryButton";
+import { Link } from "react-router-dom";
+import PrimaryButton from "../../components/Button/PrimaryButton";
 
 const Offer = () => {
   return (
@@ -12,6 +13,71 @@ const Offer = () => {
           <h1 className="text-[2.5rem] text-gray-900 text-center font-black">
             Best Offers
           </h1>
+        </div>
+        <div className="w-full h-auto flex items-center justify-between mb-10 gap-x-4 gap-y-5 flex-wrap">
+          <div className="w-[32%] h-auto bg-white rounded-2xl shadow border-b-[6px] border-gray-300 hover:border-b-[6px] hover:border-orange-500 ease-out duration-700 overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2016/11/29/04/18/hot-air-balloons-1867279_640.jpg"
+              alt=""
+              className="w-full h-[38vh] object-cover object-center rounded-2xl brightness-75"
+            />
+            <div className="px-5 py-8">
+              <Link className="text-2xl text-gray-900 font-bold mb-4 block">
+                Hot Air Ballon, Nepal
+              </Link>
+              <p className="text-base text-gray-500 font-normal mb-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aliquid ipsa officia doloremque dolore asperiores quos similique
+                laboriosam ut id! Quisquam.
+              </p>
+              <div className="w-full h-auto flex items-center justify-between">
+                <PrimaryButton>Book Now</PrimaryButton>
+                <h6 className="text-xl text-orange-600 font-bold">$180.00</h6>
+              </div>
+            </div>
+          </div>
+          <div className="w-[32%] h-auto bg-white rounded-2xl shadow border-b-[6px] border-gray-300 hover:border-b-[6px] hover:border-orange-500 ease-out duration-700 overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2019/07/19/04/19/trees-4347932_640.jpg"
+              alt=""
+              className="w-full h-[38vh] object-cover object-center rounded-2xl brightness-75"
+            />
+            <div className="px-5 py-8">
+              <Link className="text-xl text-gray-900 font-semibold mb-4 block">
+                Title Section
+              </Link>
+              <p className="text-base text-gray-400 font-normal mb-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aliquid ipsa officia doloremque dolore asperiores quos similique
+                laboriosam ut id! Quisquam.
+              </p>
+              <div className="w-full h-auto flex items-center justify-between">
+                <PrimaryButton>Book Now</PrimaryButton>
+                <h6 className="text-xl text-orange-600 font-bold">$180.00</h6>
+              </div>
+            </div>
+          </div>
+          <div className="w-[32%] h-auto bg-white rounded-2xl shadow border-b-[6px] border-gray-300 hover:border-b-[6px] hover:border-orange-500 ease-out duration-700 overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2021/04/07/02/38/waterfall-6157891_640.jpg"
+              alt=""
+              className="w-full h-[38vh] object-cover object-center rounded-2xl brightness-75"
+            />
+            <div className="px-5 py-8">
+              <Link className="text-xl text-gray-900 font-semibold mb-4 block">
+                Title Section
+              </Link>
+              <p className="text-base text-gray-400 font-normal mb-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aliquid ipsa officia doloremque dolore asperiores quos similique
+                laboriosam ut id! Quisquam.
+              </p>
+              <div className="w-full h-auto flex items-center justify-between">
+                <PrimaryButton>Book Now</PrimaryButton>
+                <h6 className="text-xl text-orange-600 font-bold">$180.00</h6>
+              </div>
+            </div>
+          </div>
         </div>
         {/* <div className="w-full h-auto flex items-center justify-center mb-6">
           <SecondaryButton>
