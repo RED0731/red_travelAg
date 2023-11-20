a0213276c989baa629b6edb7a48c0caab4c65f65
alocparker <alocparker1220@gmail.com>
2023-11-20 09:05:49 +0545

    working on the footer section

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="w-full h-auto py-16 px-20 bg-gray-200">This is Footer Section.</div>
+      <div className="w-full h-auto py-16 px-20 bg-gray-200">
+        <div className="w-[35%] h-auto bg-orange-500 p-6 rounded-xl">
+          This is the section.
+        </div>
+      </div>
     </>
   );
 };
