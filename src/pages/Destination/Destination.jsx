e0240692eac725e86b4ffff50dffcb54017384ed
alocparker <alocparker1220@gmail.com>
2023-11-27 21:04:47 +0545

    working on the destination section

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import "./Destination.css";
 import SecondaryButton from "../../components/Button/SecondaryButton";
+import Tag from "../../components/Tag/Tag";
+import Viewmore from "../../components/Viewmore/Viewmore";
 
 const Destination = () => {
   return (
     <>
       <div className="w-full h-auto py-16 px-20">
-        <div className="text-center mb-10">
-          <div className="satisfy-font text-2xl font-medium text-orange-500 text-center">
-            Popular Destinations
-          </div>
-          <h1 className="text-[2.5rem] text-gray-900 text-center font-black">
-            Top Travel Destinations
-          </h1>
-        </div>
+        <Tag tagSlog="Popular Destinations" tagTitle="Top Travel Destinations" />
         <div className="w-full h-auto flex items-center justify-between mb-10 gap-7">
           <div className="w-[30%] h-[50vh] rounded-xl overflow-hidden relative">
             <img
@@ -83,24 +78,7 @@ const Destination = () => {
             </div>
           </div>
         </div>
-        <div className="w-full h-auto flex items-center justify-center mb-6">
-          <SecondaryButton>
-            View more{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-arrow-right"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-              />
-            </svg>
-          </SecondaryButton>
-        </div>
+        <Viewmore />
       </div>
     </>
   );
