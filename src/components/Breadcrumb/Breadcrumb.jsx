2ad65eb419ef3d3b42036426985e00bb3185be9c
alocparker <alocparker1220@gmail.com>
2023-12-04 20:02:55 +0545

    adding the animate video in the website

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import TravelAg from "../../assets/Breadcrumb/travelag.gif";
 
 const Breadcrumb = () => {
   return (
     <>
       <div className="w-full h-auto">
+        <video
+          type="video/mp4"
+          autoplay
+          loop
+          muted
+          plays-inline
+          class="carousel_video"
+        >
+          <source src="../img/bg_video.mp4" />
+        </video>
         <h1 className="text-2xl text-orange-500">Breadcrumb</h1>
-        <p className="text-gray-500"> Breadcrumb grabs you to the current page</p>
+        <p className="text-gray-500">
+          {" "}
+          Breadcrumb grabs you to the current page
+        </p>
       </div>
     </>
   );
