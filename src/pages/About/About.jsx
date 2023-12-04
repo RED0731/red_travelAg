d98d24e603aa0b939419e62e239944c939434e7f
alocparker <alocparker1220@gmail.com>
2023-12-04 16:58:43 +0545

    changes the things

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,7 +4,7 @@ import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 const About = () => {
   return (
     <>
-      <div className="w-full h-auto">
+      <div className="w-full h-auto py-16 lg:px-20 md:px-16 sm:px-8 px-4">
         <Breadcrumb />
       </div>
     </>
--- src/pages/Packages/Packages.jsx
+++ src/pages/Packages/Packages.jsx
@@ -4,7 +4,7 @@ import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 const Packages = () => {
   return (
     <>
-      <div className="w-full h-auto">
+      <div className="w-full h-[34vh] bg-gray-600 py-24 lg:px-20 md:px-16 sm:px-8 px-4">
         <Breadcrumb />
       </div>
     </>
