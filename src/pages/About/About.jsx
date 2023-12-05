0d2bbfc99f738b05373aeb6424c04a1819b4da48
alocparker <alocparker1220@gmail.com>
2023-12-05 12:33:01 +0545

    completed the team section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -15,11 +15,11 @@ const About = () => {
         <div className="w-full h-auto pt-14 pb-6">
           <Tag tagSlog="Our Teams" tagTitle="Meet Expert Teams" />
           <div className="w-full h-auto flex items-center justify-between gap-7">
-            <div className="w-[30%] h-auto relative rounded-3xl overflow-hidden cursor-pointer">
+            <div className="w-[30%] h-[80vh] relative rounded-bl-3xl rounded-tl-3xl overflow-hidden cursor-pointer">
               <img
                 src="https://cdn.pixabay.com/photo/2019/03/24/22/05/man-4078827_1280.jpg"
                 alt="team member"
-                className="w-full h-[80vh] object-cover object-center"
+                className="w-full h-full object-cover object-center"
               />
               <div className="w-full h-full absolute bottom-0 left-0 team_bg flex items-end text-center px-6 py-8">
                 <div className="w-full h-auto flex items-start flex-col">
@@ -33,12 +33,12 @@ const About = () => {
               </div>
             </div>
             <div className="w-[70%] h-[80vh] relative flex items-start flex-col gap-y-7">
-              <div className="w-full h-1/2 flex items-start justify-between gap-7">
-                <div className="w-[31%] h-full relative rounded-3xl overflow-hidden cursor-pointer">
+              <div className="w-full h-[48%] flex items-start justify-between gap-7">
+                <div className="w-[31%] h-full relative overflow-hidden cursor-pointer">
                   <img
                     src="https://images.unsplash.com/photo-1626565244872-206f4c1f9e57?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="image"
-                    className="w-full h-full object-cover object-center rounded-3xl cursor-pointer"
+                    className="w-full h-full object-cover object-center cursor-pointer"
                   />
                   <div className="w-full h-full absolute bottom-0 left-0 team_bg flex items-end text-center px-6 py-8">
                     <div className="w-full h-auto flex items-start flex-col">
@@ -51,11 +51,11 @@ const About = () => {
                     </div>
                   </div>
                 </div>
-                <div className="w-[31%] h-full relative rounded-3xl overflow-hidden cursor-pointer">
+                <div className="w-[31%] h-full relative overflow-hidden cursor-pointer">
                   <img
                     src="https://images.unsplash.com/photo-1615136002804-166c5961414e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="image"
-                    className="w-full h-full object-cover object-center rounded-3xl cursor-pointer"
+                    className="w-full h-full object-cover object-center cursor-pointer"
                   />
                   <div className="w-full h-full absolute bottom-0 left-0 team_bg flex items-end text-center px-6 py-8">
                     <div className="w-full h-auto flex items-start flex-col">
@@ -68,11 +68,11 @@ const About = () => {
                     </div>
                   </div>
                 </div>
-                <div className="w-[31%] h-full relative rounded-3xl overflow-hidden cursor-pointer">
+                <div className="w-[31%] h-full relative rounded-tr-3xl overflow-hidden cursor-pointer">
                   <img
                     src="https://images.unsplash.com/photo-1587050265310-1a2d98ccce5f?q=80&w=1861&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="image"
-                    className="w-full h-full object-cover object-center rounded-3xl cursor-pointer"
+                    className="w-full h-full object-cover object-center cursor-pointer"
                   />
                   <div className="w-full h-full absolute bottom-0 left-0 team_bg flex items-end text-center px-6 py-8">
                     <div className="w-full h-auto flex items-start flex-col">
@@ -86,12 +86,12 @@ const About = () => {
                   </div>
                 </div>
               </div>
-              <div className="w-full h-1/2 flex items-start justify-between gap-7">
-                <div className="w-[69%] h-full relative rounded-3xl overflow-hidden cursor-pointer">
+              <div className="w-full h-[47%] flex items-start justify-between gap-7">
+                <div className="w-[69%] h-full relative overflow-hidden cursor-pointer">
                   <img
                     src="https://images.unsplash.com/photo-1573496267526-08a69e46a409?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="image"
-                    className="w-full h-full object-cover object-center rounded-3xl cursor-pointer"
+                    className="w-full h-full object-cover object-center cursor-pointer"
                   />
                   <div className="w-full h-full absolute bottom-0 left-0 team_bg flex items-end text-center px-6 py-8">
                     <div className="w-full h-auto flex items-start flex-col">
@@ -104,11 +104,11 @@ const About = () => {
                     </div>
                   </div>
                 </div>
-                <div className="w-[31%] h-full relative rounded-3xl overflow-hidden cursor-pointer">
+                <div className="w-[31%] h-full relative overflow-hidden rounded-br-3xl cursor-pointer">
                   <img
                     src="https://images.unsplash.com/photo-1599583287831-385c6a578044?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="image"
-                    className="w-full h-full object-cover object-center rounded-3xl cursor-pointer"
+                    className="w-full h-full object-cover object-center cursor-pointer"
                   />
                   <div className="w-full h-full absolute bottom-0 left-0 team_bg flex items-end text-center px-6 py-8">
                     <div className="w-full h-auto flex items-start flex-col">
