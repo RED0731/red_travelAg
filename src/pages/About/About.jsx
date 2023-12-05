bd1165d469ec96313f0d73f60c23cffeebe21f22
alocparker <alocparker1220@gmail.com>
2023-12-05 10:26:01 +0545

    changes the things

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -13,7 +13,17 @@ const About = () => {
         <AboutComp />
         <div className="w-full h-auto pt-14 pb-6">
           <Tag tagSlog="Our Teams" tagTitle="Meet Expert Teams" />
+          <div className="w-full h-auto flex items-center justify-between gap-7">
+            <div className="w-[25%] h-auto flex items-center flex-col gap-y-4">
+              <img
+                src="https://cdn.pixabay.com/photo/2016/11/29/04/18/hot-air-balloons-1867279_640.jpg"
+                alt="team member"
+                className="w-full h-[55vh] object-cover object-center"
+              />
+            </div>
+          </div>
         </div>
+        {/* Hiking expert, Jungle Safari expert, Beach expert, Boat expert, City tour expert, Adventure, Hot Air Ballon, Expert offroad driver, Traveller, Mountainery expert, Waterfall expert, Climbing expert, Camping expert, Food expert, Photography expert, Nature expert, Cooking expert, Cultural expert, Transportation expert, Cooking expert, Cultural expert, Transportation expert, Cooking expert, Cultural expert, Transportation expert, Cooking expert, Cultural expert, Transportation expert */}
       </div>
     </>
   );
