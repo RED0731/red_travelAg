c6b80b97dfb8b86537460fb83395bc1a871e7d94
alocparker <alocparker1220@gmail.com>
2023-12-08 08:38:57 +0545

    contact us page created

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
+import ContactPage from "../MainPage/Contact/Contact";
 
 const Contact = () => {
   return (
     <>
       <div className="w-full h-[40vh] mb-14 mt-16">
         <Breadcrumb title="Contact" breadcrumb="Home / Contact" />
+      </div>
+      <div className="w-full h-auto pb-16 lg:px-20 md:px-16 sm:px-8 px-4">
+        <ContactPage />
       </div>
     </>
   );
