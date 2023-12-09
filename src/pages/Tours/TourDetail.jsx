2fc1c5e3c895ff434eac8f390b8ae31d85c9f639
alocparker <alocparker1220@gmail.com>
2023-12-09 20:12:37 +0545

    working on the tour detail section

--- src/pages/Tours/TourDetail.jsx
+++ src/pages/Tours/TourDetail.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const TourDetail = () => {
   return (
     <>
-      <div className="w-full h-auto py-16 lg:px-20 md:px-16 sm:px-8 px-4">
-        This is the tour detail page for the selected tour here.
+      <div className="w-full h-auto">
+        <img
+          src="https://cdn.pixabay.com/photo/2016/11/14/04/36/boy-1822614_1280.jpg"
+          alt=""
+          className="w-full h-[55vh] object-cover object-center"
+        />
       </div>
     </>
   );
